eb9f737d9cbee4f2862298934e9d0b9e00b9d5a3
Camille <contact.pericat@protonmail.com>
2024-11-28 15:19:51 +0100

    gpt

--- start.py
+++ start.py
@@ -3,44 +3,44 @@ from openai import OpenAI
 
 
 
-# client = OpenAI(api_key="sk-Y3-qrmX6Y1BSewoscrQoVcCyONx-w9obysg0XzyJzyT3BlbkFJReGsbUhJ11Rd-PQ--QdlXIModydWk5tr-woCZTCFgA")
+client = OpenAI()
 
-# txtobj = st.text("Chargement en cours...")
+txtobj = st.text("Chargement en cours...")
     
-# completion = client.chat.completions.create(
-#     model="gpt-4o-mini",
-#     messages=[
-#         {"role": "user","content": "write a haiku about ai"}
-#     ]
-# )
+completion = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+        {"role": "user","content": "write a haiku about ai"}
+    ]
+)
 
-# txtobj.text(completion.choices[0].message.content)
+txtobj.text(completion.choices[0].message.content)
 
 # import streamlit as st
 
 # Texte
-st.write("Write")
-st.text("Text")
-st.header("Header")
-st.subheader("Subheader")
+# st.write("Write")
+# st.text("Text")
+# st.header("Header")
+# st.subheader("Subheader")
 
-# Input
-st.button("Button")
-st.checkbox("Checkbox")
-st.toggle("Toggle")
-st.radio("Radio", ["Première option", "Seconde option"])
-st.text_input("Text Input")
-st.audio_input("Audio Input")
-st.file_uploader("File Uploader")
-st.slider("Slider", 0, 100)
+# # Input
+# st.button("Button")
+# st.checkbox("Checkbox")
+# st.toggle("Toggle")
+# st.radio("Radio", ["Première option", "Seconde option"])
+# st.text_input("Text Input")
+# st.audio_input("Audio Input")
+# st.file_uploader("File Uploader")
+# st.slider("Slider", 0, 100)
 
-# Navigation
-pg = st.navigation([
-    st.Page("page1.py", title="First page", icon="🔥"),
-    st.Page("page2.py", title="Second page", icon="😊"),
-])
-pg.run()
+# # Navigation
+# pg = st.navigation([
+#     st.Page("page1.py", title="First page", icon="🔥"),
+#     st.Page("page2.py", title="Second page", icon="😊"),
+# ])
+# pg.run()
 
-# Media
-st.image("image.webp", width=50, caption="Image")
-st.audio("audio.wav")
+# # Media
+# st.image("image.webp", width=50, caption="Image")
+# st.audio("audio.wav")
